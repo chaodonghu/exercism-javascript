a2759977c6881d27d61500fd7724db8b48fa350d
Dong Hu <cdhu@uwaterloo.ca>
2017-07-06 17:23:50 -0400

    Complete bob exercise

--- bob/bob.js
+++ bob/bob.js
@@ -1,7 +1,21 @@
-var Bob = function () {};
+var Bob = function() {};
 
-Bob.prototype.convo = function (input) {
+Bob.prototype.hey = function(input) {
+  // If sentence contains just capital letters or is all uppercase  - Shouting.
+  // Catches scenario if asked a forceful question.
+  if (input === input.toUpperCase() && input !== input.toLowerCase())
+    return "Whoa, chill out!";
 
-  // Question
-  if ()
-}
+  // If asked a question. (sentence ends in question mark)
+  if (input.endsWith('?'))
+    return "Sure.";
+
+  // If addressed without actually saying anything, blank space.
+  if (input.trim() === '')
+    return "Fine. Be that way!";
+
+  // Anything else
+  return "Whatever.";
+  };
+
+module.exports = Bob;
